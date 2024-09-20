1b3222dd22c015dce6d74d1b67124ee075fc1343
UTakuto <taku.0307.ue@icloud.com>
2024-09-20 17:38:06 +0900

    Refactor TodoList component to manage todo items and their deletion

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,45 @@
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+
 export default function TodoList() {
-    return <div></div>;
+    const [todos, setTodos] = useState<string[]>([]);
+
+    const [newTodo, setNewTodo] = useState("");
+
+    const addTodo = () => {
+        setTodos([...todos, newTodo]);
+        setNewTodo("");
+    };
+
+    const deleteTodo = (indexToDelete: number) => {
+        setTodos(todos.filter((_, index) => index !== indexToDelete));
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={newTodo}
+                onChange={(e) => {
+                    setNewTodo(e.target.value);
+                }}
+            />
+
+            <button onClick={addTodo}>Add Todo</button>
+
+            <ul>
+                {todos.map((todo, index) => (
+                    <div>
+                        <TodoItem
+                            key={index}
+                            todo={todo}
+                            onDelete={() => {
+                                deleteTodo(index);
+                            }}
+                        />
+                    </div>
+                ))}
+            </ul>
+        </div>
+    );
 }
