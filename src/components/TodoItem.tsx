19b57c67ebc5941d6be0b0de764000c813a9a26e
UTakuto <taku.0307.ue@icloud.com>
2024-09-20 17:30:41 +0900

    Refactor TodoItem component to accept todo and onDelete props

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,3 +1,15 @@
-export default function TodoItem() {
-    return <div></div>;
+type TodoItemProps = {
+    todo: string;
+    onDelete: () => void;
+};
+
+export default function TodoItem({ todo, onDelete }: TodoItemProps) {
+    return (
+        <li>
+            {todo}
+            <button onClick={onDelete} style={{ margin: "0 10px" }}>
+                delete
+            </button>
+        </li>
+    );
 }
